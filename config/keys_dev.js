521d0f1b1de2b20709aa66a1a85f02512d9b0a49
Hovig Kalayjian <hovigkalayjian@gmail.com>
2023-03-30 18:30:07 -0400

    .env file

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,9 +1,4 @@
 // module.exports = {
-//   stripePublishableKey:
-//     "pk_test_51MTxWlJ6ot15kLDDB1g7WVmdu2Jgqw7IjV6LFj8qroeyDBmxwJARSQccbVbcaxTB6rTWpGmj5guZ4xVPf7Zw4bZ700Z9BeQDuu",
-//   stripeSecretKey:
-//     "sk_test_51MTxWlJ6ot15kLDDiKe2XEiJQa28MyCdKPmJoazPK42vJeExoAsbpqJRIiwOPYpJCxIFqpYyE5UDpihvafu7LXqK00ZjzLCkqQ",
-// };
 module.exports = {
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
